aa5aaf80708939f15c2ee3e24417c42e9aca3acb
Yonatan Lahav <jonathanla180@gmail.com>
2022-03-30 19:48:10 +0300

    Login form with functionality

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,33 +1,48 @@
-import React from 'react'
-import './LoginForm.css'
+import React from 'react';
+import './LoginForm.css';
+import { data } from './data';
+import { useState } from 'react';
+
+function handleClick() {
+    return alert('The link was clicked.');
+}
 
 const LoginForm = () => {
-    var x = '';
+
+    const [inUsername, setInUsername] = useState('');
+    const [inPassword, setInPassword] = useState('');
+
+    const handleUsername = (event) => {
+        setInUsername(event.target.value);
+    }
+
+    const handlePassword = (event) => {
+        setInPassword(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const found = data.find(({ username, password }) => username === inUsername && password === inPassword);
+        alert(found.username);
+    };
+
     return (
-        <body className="text-center vsc-initialized">
-            <form className='form-signin text-center vsc-initialized'>
-                <img className="mb-4" src="logo512.png" alt="" width="72" height="57"></img>
-                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <form className='form-signin text-center vsc-initialized' onSubmit={handleSubmit}>
+            <img className="mb-4" src="logo512.png" alt="" width="72" height="57"></img>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-                <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
-                    <label htmlFor="floatingInput">Email Address</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-
-                <div className="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"></input> Remember me
-                    </label>
-                </div>
-                <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={() => alert("clicked")}>
-                    Sign in
-                </button>
-            </form >
-        </body>
+            <div className="form-floating">
+                <input type="email" className="form-control" onChange={handleUsername} value={inUsername}></input>
+                <label htmlFor="floatingInput">Email Address</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" onChange={handlePassword} value={inPassword}></input>
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Sign in
+            </button>
+        </form >
     )
 }
 
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const data =
+    [
+        { username: 'admin@gmail.com', password: '1234' },
+        { username: 'yonatan@gmail.com', password: '12345' },
+        { username: 'roy@gmail.com', password: '123456' }
+    ];
