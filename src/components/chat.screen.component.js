43dde1a91be82b98792b104cd48a653c40da4dbb
Yonatan Lahav <jonathanla180@gmail.com>
2022-04-07 20:58:56 +0300

    split chat list and messege screen

--- src/components/chat.screen.component.js
+++ src/components/chat.screen.component.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
-import './chat.screen.css'
+import './ChatScreen.css'
+import ChatList from './ChatList'
 
 
 const ChatScreen = (props) => {
     return (
         <div className='container outer inner-chat '>
-            <div className='sidebar'>
-                <table>
-                    <tr><td><div onClick={() => alert('1')}><img src="./defualtIcon.jpg" /></div></td></tr>
-                    <tr><td><button> first chat</button></td></tr>
-                </table>
-                {/* How to insert img: */}
-                {/* <img src={URL.createObjectURL(props.user.pic)} /> */}
-
-                {/* <button className='sticky-top'>chat !!!!</button> */}
+            <div className='wrapper'>
+                <div><h1>ChatList</h1></div>
+                {/* <div><ChatList /></div> */}
+                <div><h1>ChatDisplay</h1></div>
             </div>
+            {/* <div className='sidebar'> */}
+            {/* <table> */}
+            {/* <tr><td><div onClick={() => alert('1')}><img src="./defualtIcon.jpg" /></div></td></tr> */}
+            {/* <tr><td><button> first chat</button></td></tr> */}
+            {/* </table> */}
+            {/* How to insert img: */}
+            {/* <img src={URL.createObjectURL(props.user.pic)} /> */}
+
+            {/* <button className='sticky-top'>chat !!!!</button> */}
+            {/* </div> */}
         </div>
     )
 }
