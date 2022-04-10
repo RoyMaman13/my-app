b0778853b8b2eb34fd71bd169d7ffb7d221f5980
roy maman <roey.maman@gmail.com>
2022-04-10 17:06:23 +0300

    f

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 import './ChatScreen.css'
 import ChatListTemplate from './ChatListTemplate'
 import ChatList from './ChatList'
-<<<<<<< HEAD
-import ChatContent from "./ChatContent";
-=======
 import MessageScreen from './MessageScreen'
->>>>>>> master
 
 
 const ChatScreen = (props) => {
@@ -14,20 +10,13 @@ const ChatScreen = (props) => {
         <div className='container outer inner-chat '>
             <div className='wrapper'>
                 <div>
-<<<<<<< HEAD
-                    <ChatListTemplate />
-                </div>
-                <div>
-                    <ChatContent />
-=======
                     <ChatList user={props.user} />
                 </div>
                 <div>
-                    {/* <MessageScreen user={props.user} /> */}
->>>>>>> master
+                    <MessageScreen user={props.user} />
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
--- src/components/MessageScreen.js
+++ src/components/MessageScreen.js
@@ -4,7 +4,7 @@ export const MessageScreen = (props) => {
     return (
 
         <div>
-            {/* {props.chats.map(({ nickname, pic, messegeHistory }) => {
+            {props.chats.map(({ nickname, pic, messegeHistory }) => {
                 return (
                     <div className="chatlist__items">
                         <img src={pic} alt="#" />
@@ -13,7 +13,7 @@ export const MessageScreen = (props) => {
                         <p>{messegeHistory[lastMessegeIndex].messege}</p>
                     </div>
                 );
-            })} */}
+            })}
         </div>
     )
 }
