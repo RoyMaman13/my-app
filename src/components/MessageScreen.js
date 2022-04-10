3c466fea887eb8ed56d3d99f2377d72fd5d0abdb
Yonatan Lahav <jonathanla180@gmail.com>
2022-04-10 15:57:09 +0300

    add chat list

--- src/components/MessageScreen.js
+++ src/components/MessageScreen.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
 export const MessageScreen = (props) => {
+    let messeges = props.user.chats[0];
+    let chatWith = props.user.chats[0].nickname;
+    console.log(messeges)
+    console.log(chatWith)
     return (
-
         <div>
-            {/* {props.chats.map(({ nickname, pic, messegeHistory }) => {
+            {messeges.messegeHistory.map(({ from, messege, time }) => {
+                let sender = (from !== '') ? chatWith : "Me";
                 return (
-                    <div className="chatlist__items">
-                        <img src={pic} alt="#" />
-                        {nickname}
-                        <p>{messegeHistory[lastMessegeIndex].time}</p>
-                        <p>{messegeHistory[lastMessegeIndex].messege}</p>
+                    <div>
+                        {sender + ": "}
+                        {messege}
                     </div>
                 );
-            })} */}
+            })}
         </div>
     )
 }
