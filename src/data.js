dda4084e51cde737748eedd24ec47012d97bc385
Yonatan Lahav <jonathanla180@gmail.com>
2022-04-16 14:09:58 +0300

    Merge branch 'roys-branch' into Yonatan

--- src/data.js
+++ src/data.js
@@ -7,7 +7,6 @@ export const data =
             pic: 'https://cdn-icons.flaticon.com/png/512/4140/premium/4140037.png?token=exp=1649582726~hmac=a36aa5ea446198f6b35194b7de24e040',
             chats: [
                 {
-                    id: '0',
                     username: 'yonatan@gmail.com',
                     nickname: 'Yonatan',
                     pic: 'https://img.icons8.com/fluency/344/person-male.png',
@@ -45,7 +44,6 @@ export const data =
                     ]
                 },
                 {
-                    id: '1',
                     username: 'roy@gmail.com',
                     nickname: 'Roy',
                     pic: 'https://img.icons8.com/officel/452/person-male.png',
@@ -68,42 +66,6 @@ export const data =
                     ]
                 },
                 {
-                    id: '2',
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
                     username: 'Neta@gmail.com',
                     nickname: 'Neta',
                     pic: 'https://img.icons8.com/officel/452/person-male.png',
@@ -123,108 +85,7 @@ export const data =
             password: '123456',
             nickname: 'roy',
             pic: 'https://cdn-icons.flaticon.com/png/512/3001/premium/3001764.png?token=exp=1649584195~hmac=a8b7ddddc6a41e57a068623ebbf6ef70',
-            chats: [
-                {
-                    username: 'yonatan@gmail.com',
-                    nickname: 'Yonatan',
-                    pic: 'https://img.icons8.com/fluency/344/person-male.png',
-                    messegeHistory: [
-                        {
-                            type: 'text',
-                            from: '',
-                            messege: 'Hello',
-                            time: '16:00, 10/4/2022'
-                        },
-                        {
-                            type: 'photo',
-                            from: 'other',
-                            messege: 'https://cdn-icons-png.flaticon.com/512/5375/5375879.png',
-                            time: '16:10, 10/4/2022'
-                        },
-                        {
-                            type: 'text',
-                            from: '',
-                            messege: '    Lorem, ipsum dolor sit veniam nostrum. Voluptatibus pariatur quia aliquid placeat ex, dicta culpa, repellendus animi libero fugit eum consequatur impedit ut a, cum perspiciatis non. Quasi quos exercitationem ducimus sapiente cupiditate non fugiat earum dignissimos illum voluptate id temporibus vel illo fugit corporis, autem labore reprehenderit deleniti sit, tempora repudiandae maxime. Nemo ut quam omnis repellendus modi amet error ullam impedit, quaerat commodi et. Ab, doloribus.',
-                            time: '16:11, 10/4/2022'
-                        },
-                        {
-                            type: 'photo',
-                            from: '',
-                            messege: 'https://cdn-icons-png.flaticon.com/512/5375/5375879.png',
-                            time: '16:10, 10/4/2022'
-                        },
-                        {
-                            type: 'text',
-                            from: 'other',
-                            messege: 'Hello',
-                            time: '16:00, 10/4/2022'
-                        }
-                    ]
-                },
-                {
-                    username: 'roy@gmail.com',
-                    nickname: 'Roy',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: [
-                        {
-                            from: '',
-                            messege: 'Hello',
-                            time: '16:00, 10/4/2022'
-                        },
-                        {
-                            from: 'other',
-                            messege: 'How are you ?',
-                            time: '16:10, 10/4/2022'
-                        },
-                        {
-                            from: '',
-                            messege: 'good',
-                            time: '16:11, 10/4/2022'
-                        }
-                    ]
-                },
-                {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }, {
-                    username: 'Neta@gmail.com',
-                    nickname: 'Neta',
-                    pic: 'https://img.icons8.com/officel/452/person-male.png',
-                    messegeHistory: []
-                }
-            ]
+            chats: []
 
         },
         {
@@ -238,7 +99,8 @@ export const data =
             username: 'biba1@gmail.com',
             password: '1',
             nickname: 'Biba',
-            pic: 'https://cdn-icons-png.flaticon.com/512/5375/5375879.png'
+            pic: 'https://cdn-icons-png.flaticon.com/512/5375/5375879.png',
+            chats: []
         }
 
     ];
